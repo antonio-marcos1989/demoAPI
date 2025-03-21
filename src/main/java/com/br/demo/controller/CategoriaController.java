8a88b48b7eb47a4146ac4eb507161b75c5378e51
Antonio Marcos Alves Morais <115155873+antonio-marcos1989@users.noreply.github.com>
2025-03-21 20:09:00 -0300

    Code Refactored Part 2

--- src/main/java/com/br/demo/controller/CategoriaController.java
+++ src/main/java/com/br/demo/controller/CategoriaController.java
@@ -2,7 +2,6 @@ package com.br.demo.controller;
 
 import com.br.demo.dto.CategoriaDTO;
 import com.br.demo.service.CategoriaService;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
